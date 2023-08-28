7f43f5ac93a76be2bc7e1cf0d0a9ec0e7e2be85b
Herbert Schiller <hbschiller@gmail.com>
2023-08-28 08:51:33 +0000

    testing test with jest

---
+++ tests/calculadora.test.js
@@ -0,0 +1,13 @@
+test("espero que 1 seja 1", () => {
+  expect(1).toBe(1);
+});
+
+// test("nome do test", ()=>{
+//     console.log("oi")
+// });
+
+// test("outro test", callbackFunction)
+
+// function callbackFunction(){
+//     console.log("funçao chamada")
+// }
